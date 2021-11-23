06f42d0b3d131d1b946c22dacd95095c98a6aeef
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2021-11-23 16:34:52 +0100

    add coment

--- modules/fs2kafka/src/main/scala/com/spike/fs2kafka/kafka/Producer.scala
+++ modules/fs2kafka/src/main/scala/com/spike/fs2kafka/kafka/Producer.scala
@@ -29,6 +29,9 @@ object Producer {
       .resource(producerSettings)
       .map(kafkaProducer =>
         new Producer[F, Message[K, V]] {
+          //The function returns two effects, e.g. IO[IO[...]],
+          //where the first effect puts the records in the producer's buffer,
+          //and the second effects waits for the records to have been sent.
           def sendMessage(message: Message[K, V]): F[Unit] =
             kafkaProducer.produce(ProducerRecords.one(ProducerRecord(topic, message.key, message.value))).flatten.void
       })
